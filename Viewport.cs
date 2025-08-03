0b5b16c174a05525bdda9c4e2a4a3b1e47c41d88
danielrippa <621091+danielrippa@users.noreply.github.com>
2025-08-03 15:16:43 -0300

    .

--- Viewport.cs
+++ Viewport.cs
@@ -15,34 +15,34 @@ namespace Console {
       get => GetStdOutHandle();
     }
 
+    private SMALL_RECT Window {
+      get => GetBufferInfo(Handle).srWindow;
+    }
+
     public int Left {
-      get => GetBufferInfo(Handle).srWindow.Left;
+      get => Window.Left;
       set => MoveTo(Top, value);
     }
 
     public int Top {
-      get => GetBufferInfo(Handle).srWindow.Top;
+      get => Window.Top;
       set => MoveTo(value, Left);
     }
 
     public int Right {
-      get => GetBufferInfo(Handle).srWindow.Right;
+      get => Window.Right;
     }
     
     public int Bottom {
-      get => GetBufferInfo(Handle).srWindow.Bottom;
+      get => Window.Bottom;
     }
 
     public int Width {
-      get => GetBufferInfo(Handle).srWindow.Right - GetBufferInfo(Handle).srWindow.Left + 1;
+      get => Right - Left + 1;
     }
 
     public int Height {
-      get
-      {
-        var bufferInfo = GetBufferInfo(Handle);
-        return bufferInfo.srWindow.Bottom - bufferInfo.srWindow.Top + 1;
-      }
+      get => Bottom - Top + 1;
     }
 
     public int MaxWidth {
@@ -90,14 +90,6 @@ namespace Console {
       return SetConsoleWindowInfo(Handle, true, ref rect);
     }
     
-    public int GetLargestWidth() {
-      return GetBufferInfo(Handle).dwMaximumWindowSize.X;
-    }
-
-    public int GetLargestHeight() {
-      return GetBufferInfo(Handle).dwMaximumWindowSize.Y;
-    }
-
   }
 
 }
